6b6ca135033c3cdaf0044ac137086ecbbc62273f
Pasindu Malinda Waidyarathne <108586125+PasinduWaidyarathna@users.noreply.github.com>
2024-06-25 13:07:49 +0530

    Update EditTopicForm.tsx

--- components/EditTopicForm.tsx
+++ components/EditTopicForm.tsx
@@ -14,7 +14,7 @@ export default function EditTopicForm({ id, title, description }: { id: string; 
     e.preventDefault();
 
     try {
-      const res = await fetch(`http://localhost:3001/api/topics/${id}`, {
+      const res = await fetch(`http://localhost:3000/api/topics/${id}`, {
         method: "PUT",
         headers: {
           "Content-type": "application/json",
